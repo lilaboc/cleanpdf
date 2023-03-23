180c602ce32301494423c0e0f33a144a52fc12e4
Zhou Wei <wei.zhou@edoovo.com>
2023-03-23 14:04:58 +0800

    initial

--- cleanpdf/__init__.py
+++ cleanpdf/__init__.py
@@ -1,24 +1,35 @@
 import sys
 import pathlib
 import os
+from typing import List
+
+from PIL import ImageGrab
 from pdf2image import convert_from_path
+
+
+def process(filename):
+    try:
+        pages = convert_from_path(filename)
+        pages = [page.convert("L").point(lambda p: 255 if p > 230 else p) for page in pages]
+        path = pathlib.Path(filename)
+        path = path.with_stem(path.stem + "_cleaned")
+        pages[0].save(
+            path, "PDF", resolution=100.0, save_all=True, append_images=pages[1:]
+        )
+        for i in [filename, path]:
+            os.system('start "{}"'.format(i))
+    except Exception as exp:
+        print(exp)
+        input()
 
 
 def main():
     if len(sys.argv) > 1:
-        try:
-            pages = convert_from_path(sys.argv[1])
-            pages = [page.convert("L").point(lambda p: 255 if p > 230 else p) for page in pages]
-            path = pathlib.Path(sys.argv[1])
-            path = path.with_stem(path.stem + "_cleaned")
-            pages[0].save(
-                path, "PDF", resolution=100.0, save_all=True, append_images=pages[1:]
-            )
-            for i in [sys.argv[1], path]:
-                os.system('start "{}"'.format(i))
-        except Exception as exp:
-            print(exp)
-            input()
+        process(sys.argv[1])
+    else:
+        im = ImageGrab.grabclipboard()
+        if isinstance(im, List) and isinstance(im[0], str) and os.path.exists(im[0]):
+            process(im[0])
 
 
 if __name__ == '__main__':
