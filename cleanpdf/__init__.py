436284844ed7f5fa878b8f6cac1239e0166cd06b
Zhou <lilaboc.cn@gmail.com>
2023-03-23 22:30:41 +0800

    otsu algorithm

--- cleanpdf/__init__.py
+++ cleanpdf/__init__.py
@@ -3,18 +3,47 @@ import pathlib
 import os
 from typing import List
 
+import numpy as np
 from PIL import ImageGrab
 from pdf2image import convert_from_path
+
+
+# https://zhuanlan.zhihu.com/p/444684653
+def otsu(page):
+    img = np.array(page)
+    h, w = img.shape[:2]
+    threshold_t = 0
+    max_g = 0
+    for t in range(255):
+        print(t)
+        front = img[img < t]
+        back = img[img >= t]
+        front_p = len(front) / (h * w)
+        back_p = len(back) / (h * w)
+        front_mean = np.mean(front) if len(front) > 0 else 0.
+        back_mean = np.mean(back) if len(back) > 0 else 0.
+        g = front_p * back_p * ((front_mean - back_mean) ** 2)
+        if g > max_g:
+            max_g = g
+            threshold_t = t
+    print('thredshold is {}'.format(threshold_t))
+    return threshold_t
 
 
 def process(filename):
     try:
         pages = convert_from_path(filename)
-        pages = [page.convert("L").point(lambda p: 255 if p > 230 else p) for page in pages]
+        # pages = [page.convert("L").point(lambda p: 255 if p > 230 else p) for page in pages]
+        result = []
+        for page in pages:
+            page = page.convert("L")
+            threshold = otsu(page)
+            page = page.point(lambda p: 255 if p > threshold else p)
+            result.append(page)
         path = pathlib.Path(filename)
         path = path.with_stem(path.stem + "_cleaned")
-        pages[0].save(
-            path, "PDF", resolution=100.0, save_all=True, append_images=pages[1:]
+        result[0].save(
+            path, "PDF", resolution=100.0, save_all=True, append_images=result[1:]
         )
         for i in [filename, path]:
             os.system('start "" "{}"'.format(i))
